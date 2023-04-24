a4dc4e993838ad9cb498d5dd7392373febab2467
Pranav K <pranavkovvammal@croswer.com>
2023-04-24 19:52:24 +0530

    fix

--- lib/ui/screens/service_request_screen.dart
+++ lib/ui/screens/service_request_screen.dart
@@ -469,8 +469,7 @@ class _ServiceRequestCardState extends State<ServiceRequestCard> {
                   ? Colors.transparent
                   : Colors.grey,
             ),
-            widget.serviceRequestDetails['status'] == 'accepted' &&
-                    widget.serviceRequestDetails['payment_status'] == 'pending'
+            widget.serviceRequestDetails['status'] == 'accepted'
                 ? CustomActionButton(
                     iconData: Icons.payment_outlined,
                     label: 'Pay Now',
